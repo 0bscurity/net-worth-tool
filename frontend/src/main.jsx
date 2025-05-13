1c1885da0d52b8e6703b8d0da207b828412c6577
0bscurity <andrew@isifederal.com>
2025-05-13 16:28:21 -0400

    auth0 config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")).render(
       authorizationParams={{
         redirect_uri: `${window.location.origin}/dashboard`,
         audience: auth0Audience,
-        scope: "openid profile email",
+        scope: "openid profile email offline_access",
       }}
       cacheLocation="localstorage"
       useRefreshTokens={true}
